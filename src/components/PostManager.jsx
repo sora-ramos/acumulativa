0c40f6892ab44d0f34a60cd1fbc3fe39339c0e83
sora-ramos <slramos.c11@gmail.com>
2025-07-07 23:03:19 -0400

    tercer commit

--- src/components/PostManager.jsx
+++ src/components/PostManager.jsx
@@ -1,14 +1,29 @@
 import { useState } from "react";
 import PostForm from "./PostForm";
 
-const PostManager = () => {
-    const [lista, setLista] = useState([]);
 
-    return(
-        <>
-            <PostForm/>
-        </>
-    )
-}
+const TaskManager = () => {
+    const [tareas, setTareas] = useState([]); 
+
+    const addTarea = (tarea) => {
+        const esta = tareas.some(t => tarea.titulo === t.titulo )
+        if(esta){
+            alert('Titulo ya existe')
+            return
+        }
+        setTareas([...tareas, tarea]);
+
+        alert('Bien hecho!')
+    };
+
+    
+
+    return (
+    <>
+        <PostForm addTarea={addTarea} />
+        
+    </>
+);
+};
 
 export default PostManager;
